b673d04e81c7059b43d8435e64f43f2f4c7a2b17
marksaravi <mark.saravi@gmail.com>
2022-05-31 05:29:45 +0100

    adding constructor

---
+++ devices/tftlcd/tftlcd.go
@@ -0,0 +1,4 @@
+package tftlcd
+
+type TFTLCD interface {
+}
--- hardware/ili9341/ili9341.go
+++ hardware/ili9341/ili9341.go
@@ -5,3 +5,9 @@ import "periph.io/x/conn/v3/spi"
 type device struct {
 	conn spi.Conn
 }
+
+func NewILI9341(conn spi.Conn) *device {
+	return &device{
+		conn: conn,
+	}
+}
