8dee1b478091359643233a503a6e08e4d8bf68d3
marksaravi <mark.saravi@gmail.com>
2022-05-31 12:18:40 +0100

    tftlcd

--- devices/tftlcd/tftlcd.go
+++ devices/tftlcd/tftlcd.go
@@ -1,4 +1,48 @@
 package tftlcd
 
+import "github.com/marksaravi/fonts-go/fonts"
+
+type Rotation int
+type RGB565 uint16
+
+const (
+	BLACK  RGB565 = 0b0
+	GREEN  RGB565 = 0x003f
+	BLUE   RGB565 = 0x1f << 6
+	RED    RGB565 = 0x1f << 11
+	WHITE  RGB565 = RED | GREEN | BLUE
+	YELLOW RGB565 = RED | GREEN
+)
+
+const (
+	ROTATION_0   Rotation = 0
+	ROTATION_90  Rotation = 1
+	ROTATION_180 Rotation = 2
+	ROTATION_270 Rotation = 3
+)
+
+const ()
+
 type TFTLCD interface {
+	Update()
+	ScreenWidth() int
+	ScreenHeight() int
+
+	// Drawing methods
+	Clear(backgroundColor RGB565)
+	Pixel(x, y float64, color RGB565)
+	Line(x1, y1, x2, y2 float64, color RGB565)
+	Rectangle(x1, y1, x2, y2 float64, color RGB565)
+	Circle(x, y, radius float64, color RGB565)
+	FillRectangle(x1, y1, x2, y2 float64, color RGB565)
+	FillCircle(x, y, radius float64, color RGB565)
+
+	// Printing methods
+	MoveCursor(x, y int)
+	SetFont(font fonts.BitmapFont)
+	SetFontColor(color RGB565)
+	SetFontBackgroundColor(color RGB565)
+	SetLineHeight(height int)
+	WriteChar(char byte) error
+	Write(text string)
 }
--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/marksaravi/devices-go
 go 1.17
 
 require periph.io/x/conn/v3 v3.6.10
+
+require github.com/marksaravi/fonts-go v0.1.0 // indirect
